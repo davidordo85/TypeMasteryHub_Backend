2458611d1af13aa5d4f30dd75d83116f94c6f9f7
davidordo85 <davidordonezescacena@hotmail.es>
2024-01-27 19:36:18 +0100

    Route to retrieve all course names and another route to retrieve the complete topic by name.

--- routes/api/Courses.js
+++ routes/api/Courses.js
@@ -8,11 +8,40 @@ const TypeMasterHubCourse = require('../../models/TypeMasterHubCourse');
 /**
  * GET /api/v1/course
  */
-
 router.get('/', async function (req, res, next) {
   try {
     const result = await TypeMasterHubCourse.find();
-    res.status(200).json({ success: true, result: result });
+
+    // Mapear para obtener solo los nombres de los temas
+    const topicNames = result.map(course =>
+      course.topics.map(topic => ({ name: topic.name })),
+    );
+
+    res.status(200).json({ success: true, result: topicNames.flat() });
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * GET /api/v1/course/:name
+ */
+
+router.get('/:name', async function (req, res, next) {
+  try {
+    const course = await TypeMasterHubCourse.findOne({
+      'topics.name': req.params.name,
+    });
+
+    if (!course) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Tema no encontrado' });
+    }
+
+    const topic = course.topics.find(topic => topic.name === req.params.name);
+
+    res.status(200).json({ success: true, result: topic });
   } catch (error) {
     next(error);
   }
